704d05701afc7310cb416365ddddc0694f30f2c9
Daniel <dani89rosa@gmail.com>
2023-01-04 11:24:40 +0100

    4/1/23

---
+++ 9-sonidoAve/script.js
@@ -0,0 +1,28 @@
+// const sounds = ["gorrion", "mirlo", "petirrojo", "urrraca"]
+
+const sounds = ['gorrion', 'mirlo', 'petirrojo', 'urraca']
+
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
